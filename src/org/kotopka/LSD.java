319b0a07349ccb3241ab5248dde57b142f2f8903
John Kotopka <jkotopka@gmail.com>
2021-04-03 12:54:12 -0500

    update LSD.java, add sortExpandedExplanation() method

--- src/org/kotopka/LSD.java
+++ src/org/kotopka/LSD.java
@@ -24,12 +24,49 @@ public class LSD {
             // distribute
             for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
+
+//                char current = a[i].charAt(d);
+//                int index = count[current];
+//                aux[index] = a[i];
+//                index++;
             }
 
             // copy back
             for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
             }
+        }
+    }
+
+    public static void sortExpandedExplanation(String[] a, int w) {
+        // sort a[] on w characters
+        int n = a.length;
+        int R = 256; // extended ASCII radix
+        String[] aux = new String[n];
+
+        for (int d = w - 1; d >= 0; d--) {
+            int[] count = new  int[R + 1];
+
+            // compute freq. counts
+            for (String s : a) {
+                char current = s.charAt(d);
+                count[current + 1]++;
+            }
+
+            // transform counts to indices
+            for (int r = 0; r < R; r++) {
+                count[r + 1] += count[r];
+            }
+
+            // distribute
+            for (int i = 0; i < n; i++) {
+                char current = a[i].charAt(d);
+                aux[count[current]++] = a[i];
+            }
+
+            // copy back
+            System.arraycopy(aux, 0, a, 0, n);
+
         }
     }
 
@@ -55,7 +92,7 @@ public class LSD {
             System.out.println(s);
         }
 
-        sort(licensePlates, 7);
+        sortExpandedExplanation(licensePlates, 7);
 
         System.out.println("\nSorted:");
         for (String s : licensePlates) {
